404fb5e9f2a01c3df3bef2d757d479fa7e43cda2
Gabriel Luís da Silva <officialmercurymusicdj@gmail.com>
2022-04-05 22:19:28 -0300

    uno

--- client/index.js
+++ client/index.js
@@ -1,5 +1,3 @@
-console.log("JOGO DE UNO por Gabriel Luís da Silva");
-
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -7,34 +5,53 @@ const readline = require('readline').createInterface({
 
 var colors = require('colors/safe');
 
+const config = require('./config.json');
+
+const timeout = async (ms) =>{
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    })
+}
+
+const getPrefix = () => {
+    const d = new Date();
+    return colors.gray(d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1") + " " + colors.red(colors.bold("[UNO do mercúrio ツ] ")));
+}
+
+function LogConsole(str){
+    console.log(getPrefix() + str);
+}
+
+LogConsole("JOGO DE UNO por Gabriel Luís da Silva");
+
 const io = require('socket.io-client');
 
 let socket;
 
-function Question(str){
-    return new Promise((resolve) => {
-        readline.question(str + "\n", (answer) => resolve(answer));
+function Question(str, after){
+    if(!after) after = "";
+    return new Promise(async (resolve) => {
+        await timeout(500);
+        readline.question(getPrefix() + str + "\n\n" + colors.gray(after), (answer) => resolve(answer));
     })
 }
 
 async function Start(){
-    const name = await Question("Digite o seu nome de exibição");
+    const name = await Question("Digite o seu nome de exibição", "Seu nome: ");
 
-    const roomID = await Question("Digite o código da sala");
+    const roomID = await Question("Digite o código da sala", "Código da sala: ");
 
-    console.log("Conectando-se a sala " + roomID + "...");
+    LogConsole("Conectando-se a sala " + roomID + "...");
 
-    socket = io.connect("http://10.67.74.64:3000", {query: {
+    socket = io.connect(config.ip, {query: {
         roomID,
         name
     }});
 
     Events(socket);
     
-}
-
-function LogConsole(str){
-    console.log(colors.red("[UNO] ") + str);
 }
 
 function FormatCard(card){
@@ -50,12 +67,16 @@ function FormatCard(card){
         case "y": ret = colors.yellow(ret); break;
         case "b": ret = colors.blue(ret); break;
         case "g": ret = colors.green(ret); break;
+        case "+": ret = colors.gray(ret); break;
+        case "w": ret = colors.gray(ret); break;
     }
 
     return ret;    
 }
 
 function IsAValidCard(card){
+    if(card == "uno" || card == "draw" || card == "show" || card == "challenge") return true;
+
     if(card.length != 2){
         return false;
     }
@@ -63,17 +84,35 @@ function IsAValidCard(card){
     let color = card[0];
     let number = card[1];
 
-    if(color != "r" && color != "y" && color != "g" && color != "b") return false;
+    if(color != "r" && color != "y" && color != "g" && color != "b" && color != "+" && color != "w") return false;
 
     if(isNaN(number)) return false;
 
     return true;
 }
 
-async function Play(currentCard){
-    let play = await Question("Escolha a carta que você vai jogar");
+function Commands(cmd){
+    switch(cmd){
+        case "help":
+            LogConsole("Comandos do UNO:");
+            LogConsole(colors.red("help") + " - Mostra os comandos do UNO");
+            LogConsole(colors.red("show") + " - Mostra as suas cartas atuais");
+            LogConsole(colors.red("draw") + " - Compra uma carta");
+            LogConsole(colors.red("uno") + " - Use para não levar punição antes de jogar sua penúltima carta");
+        return true;
+        default: return false;
+    }
+}
 
-    if(IsAValidCard){
+async function Play(currentCard){
+    let play = await Question("Escolha a carta que você vai jogar", "Carta para jogar: ");
+
+    if(Commands(play)) {
+        Play(play);
+        return;
+    }
+
+    if(IsAValidCard(play)){
         socket.emit("play", play);
     }else {
         LogConsole("Jogada inválida.");
@@ -84,7 +123,23 @@ async function Play(currentCard){
 
 function Events(socket){
     socket.on("disconnect", () => {LogConsole("Desconectado!");});
-    socket.on("join", (userData) => {LogConsole("'" +userData.name + "' entrou na sala!")})
+    socket.on("join", (userData, started) => {
+        if(!started) LogConsole("" + colors.gray(userData.name) + " entrou na sala!")
+        else LogConsole("" + colors.gray(userData.name) + " está espectando!")
+    });
+    socket.on("disconnected", (userData) => {
+        LogConsole("" + colors.gray(userData.name) + " saiu da sala!")
+    })
+    socket.on("showed", (currentCard) => {
+        LogConsole(colors.white("Carta atual: ") + FormatCard(currentCard));
+        Play(currentCard);
+    })
+    socket.on("match restart", () => {
+        LogConsole("A partida vai recomeçar!")
+    });
+    socket.on("match draw", () => {
+        LogConsole("Empate! Não há vencedores! ");
+    })
     socket.on("left", (left) => {LogConsole("Faltam " + left + " jogador(es) para iniciar a partida!")})
     socket.on("starting", (countdown) => {LogConsole("Iniciando em " + countdown + " segundo(s)")})
     socket.on("show cards", (data) => {
@@ -96,18 +151,93 @@ function Events(socket){
         LogConsole("Suas cartas são: " + arr);
 
     });
+    socket.on("player play draw", (who, draw, to) => {
+        LogConsole(colors.gray(who) + " jogou uma carta de compra de " + draw + " cartas para " + to.name);
+    });
+    socket.on("round choosing color", (name) => {
+        LogConsole(colors.gray(name) + " está escolhendo a próxima cor...");
+    })
+    socket.on("round choose color", () => {
+        const again = async () => {
+            let nextColor = await Question("Escolha uma cor (Azul: b, Amarelo: y, Vermelho: r ou Verde: g)", "Próxima cor: ");
+            if(nextColor != "b" && nextColor != "y" && nextColor != "r" && nextColor != "g") return await again();
+            else{
+                socket.emit("choose color", nextColor);
+            }
+        }
+        again();
+    })
+    socket.on("match win", (winner) => {
+        LogConsole("Temos um vencedor!");
+        for(let i = 0; i < winner.length; i++){
+            LogConsole((i + 1) + "° - " + winner[i].name + " (" + winner[i].cards + " carta(s))");
+        }
+    })
+    socket.on("choosed color", (color, name) => {
+        let col = "";
+        switch(color){
+            case "b": col = colors.blue("azul"); break;
+            case "r": col = colors.red("vermelho"); break;
+            case "g": col = colors.green("verde"); break;
+            case "y": col = colors.yellow("amarelo"); break;
+        }
+        LogConsole(colors.gray(name) + " escolheu a cor " + col);
+    })
+    socket.on("challenge win", (who, challenged) => {
+        LogConsole(colors.gray(who) + " desafiou " + colors.gray(challenged) + " e venceu! Agora " + colors.gray(challenged) + " terá que comprar 6 cartas!");
+    })
+    socket.on("challenge defeat", (who, challenged) => {
+        LogConsole(colors.gray(who) + " desafiou " + colors.gray(challenged) + " e perdeu! Agora " + colors.gray(who) + " terá que comprar 6 cartas!");
+    })
+    socket.on("drawed", (user) => {
+        LogConsole(colors.gray(user) + " comprou uma carta.");
+    })
+
+    socket.on("round another", (player, currentCard) => {
+        LogConsole(colors.white("Agora é a vez de ") + colors.yellow(player));
+        LogConsole(colors.white("Carta atual: ") + FormatCard(currentCard));
+
+    });
+
     socket.on("round your", (currentCard) => {
+
         LogConsole("É a sua vez de jogar. A carta atual é " + FormatCard(currentCard));
+        LogConsole(colors.gray("Caso você não tenha uma carta, digite draw para comprar uma nova carta"));
 
         Play(currentCard);
     });
 
-    socket.on("cantplay", (currentCard) => {LogConsole("Você não pode jogar essa carta!"); Play(currentCard)});
+    socket.on("cantplay", (currentCard, isUno) => {
+        if(isUno){
+            LogConsole(colors.red("Você não pode falar uno agora!")); 
+        }else {
+            LogConsole(colors.red("Você não pode jogar essa carta!")); 
+        }
+        Play(currentCard);
+    });
+
+    socket.on("uno", (currentCard) => {
+        LogConsole(colors.rainbow("Você disse UNO!")); 
+        Play(currentCard);
+    })
+
+    socket.on("p uno", (player) => {
+        LogConsole(colors.gray(player) + colors.rainbow(" disse UNO!")); 
+    })
+
+    socket.on("p didnt uno", (player) => {
+        LogConsole(colors.gray(player) + colors.red(" não disse UNO e portanto foi obrigado a comprar duas cartas!")); 
+    })
+
+    socket.on("didnt uno", () => {
+        LogConsole(colors.red("Você não disse UNO e portanto foi obrigado a comprar duas cartas!")); 
+    })
+
     socket.on("player play", (name, card) => {
-        LogConsole(name + " jogou a carta " + FormatCard(card));
+        LogConsole(colors.white(name) + colors.grey(" jogou a carta ") + FormatCard(card));
     });
     socket.on("draw", (drawedCard) => {
-        LogConsole("Você pegou uma carta " + drawedCard);
+        LogConsole(colors.white("Você pegou uma carta ") + FormatCard(drawedCard));
     })
 }
 
--- server/index.js
+++ server/index.js
@@ -5,8 +5,70 @@ const io = require("socket.io")(httpServer, {
 
 const uuid = require('uuid');
 
-const minUsersStart = 1;
-const playerNumberCards = 7;
+const minUsersStart = 2;
+const playerNumberCards = 1;
+const numOfSameCardsPerDeck = 2;
+const numOfDrawFourPerDeck = 4;
+const numOfDrawTwoPerDeck = 8;
+const numOfWildPerDeck = 8;
+const countdownToStart = 20;
+
+function shuffleArr(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function getRandomDeck(){
+  let deck = [];
+
+  deck.push(new Carta(0, 1));
+
+  //deck
+
+  for(let i = 0; i < numOfSameCardsPerDeck; i++){
+    for(let color = 0; color < 4; color ++){
+      for(let number = 1; number <= 9; number++){
+        deck.push(new Carta(color, number));
+      }
+    }
+  }
+
+  //+4
+  for(let i = 0; i < numOfDrawFourPerDeck; i++){
+    deck.push(new Carta(4, 4));
+  }
+
+  //+2
+  for(let i = 0; i < numOfDrawTwoPerDeck; i++){
+    deck.push(new Carta(4, 2));
+  }
+
+    //wild
+    for(let i = 0; i < numOfWildPerDeck; i++){
+      deck.push(new Carta(5, 0));
+    }
+
+  for(let shuffle = 0; shuffle < 2; shuffle++){
+    deck = shuffleArr(deck);
+  }
+
+  console.log(deck);
+
+  return deck;
+}
 
 function randomNumber(min, max){
   return Math.floor((Math.random() * (max - min + 1)) + min);
@@ -20,9 +82,19 @@ function ConvertCard(color, number){
     case "g": formatColor = 1; break;
     case "b": formatColor = 2; break;
     case "y": formatColor = 3; break;
+    case "+": formatColor = 4; break;
+    case "w": formatColor = 5; break;
   }
 
   return new Carta(formatColor, number);
+}
+
+function Timeout(ms){
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, ms);
+  })
 }
 
 class Carta {
@@ -39,6 +111,8 @@ class Carta {
       case 1: formatColor = "g"; break;
       case 2: formatColor = "b"; break;
       case 3: formatColor = "y"; break;
+      case 4: formatColor = "+"; break;
+      case 5: formatColor = "w"; break;
     }
 
     return formatColor + "" + this.number;
@@ -51,6 +125,9 @@ class Room {
     this.users = new Map();
     this.currentCard = false;
     this.currentTurn = 0;
+    this.started = false;
+    this.roomDeck = false;
+    this.roomDeckIndex = 0;
   }
 
   JoinUser(userID, socket){
@@ -58,10 +135,33 @@ class Room {
     else {
       this.users.set(userID, socket);
 
-      io.to(this.roomID).emit("join", socket.data);
+      this.emit("join", socket.data, this.started);
+
+      if(this.started){
+        socket.spectating = true;
+      }else{
+        socket.spectating = false;
+      }
 
       this.CanStart();
     }
+  }
+
+  DisconnectUser(socket){
+    if(socket.data){
+      this.users.delete(socket.data.uniqueID);
+      this.emit("disconnected", socket.data);
+
+      this.CheckDraw();
+    }
+  }
+
+  emit(eventName, ...args){
+    io.to(this.roomID).emit(eventName, ...args);
+  }
+
+  emitExclude(socket, eventName, ...args){
+    if(socket) socket.to(this.roomID).emit(eventName, ...args);
   }
 
   UserArray(){
@@ -73,6 +173,16 @@ class Room {
     }
 
     return arr;
+  }
+
+  Restart(){
+    this.currentCard = false;
+    this.currentTurn = 0;
+    this.started = false;
+
+    this.emit("match restart");
+
+    this.CanStart();
   }
 
   GetClientCards(user){
@@ -89,10 +199,14 @@ class Room {
 
     for(let i = 0; i < everyone.length; i++){
       let user = everyone[i].value;
-      if(!user.cards) user.cards = [];
+
+      user.cards = [];
+
       for(let i2 = 0; i2 < playerNumberCards; i2++){
-        user.cards.push(this.GetRandomCard());
+        user.cards.push(this.GetDeckCard());
       }
+
+      user.spectating = false;
 
       this.ShowCards(user);
     }
@@ -102,32 +216,163 @@ class Room {
     if(user) user.emit("show cards", this.GetClientCards(user));
   }
 
-  StartMatch(){
-    this.currentCard = this.GetRandomCard();
-    this.GiveCardsToEveryone();
-    this.currentTurn = -1;
-    this.PassTurn(true);
-  }
-
-  PassTurn(first){
-    this.currentTurn++;
-    if(this.currentTurn == this.users.size) this.currentTurn = 0;
-
-    let playerRound = this.UserArray()[this.currentTurn].value;
-    if(!playerRound) this.PassTurn();
-    else {
-      playerRound.emit("round your", this.currentCard.id());
-      if(!first) this.ShowCards(playerRound);
+  FirstCard(){
+    let card = this.GetDeckCard();
+    if(card.color >= 4) this.FirstCard();
+    else{
+      this.currentCard = card;
     }
   }
 
+  StartMatch(){
+    this.started = true;
+    this.GiveCardsToEveryone();
+    this.currentTurn = -1;
+    this.roomDeck = getRandomDeck();
+    this.roomDeckIndex = 0;
+    this.sentido = randomNumber(0, 4);
+    this.FirstCard();
+    this.PassTurn(true);
+  }
+
+  PlayingUsers(){
+    let ret = [];
+    let arr = this.UserArray();
+    for(let i = 0; i < arr.length; i++){
+      if(!arr[i]) continue;
+      if(!arr[i].value.spectating) ret.push(arr[i]);
+    }
+
+    return ret;
+  }
+
+  async CheckDraw(){
+    if(this.PlayingUsers() <= 1){
+      this.emit("match draw");
+      await Timeout(5000);
+      this.Restart();
+      return true;
+    }
+
+    return false
+  }
+
+  async PassTurn(first){
+    await Timeout(2000);
+
+    //draw
+
+    if(await this.CheckDraw()) return;
+
+    let lastPass = false;
+
+    const fnc = async () => {
+      this.currentTurn = this.currentTurn + (this.sentido % 2 == 0 ? (1) : (-1));
+
+      if(this.currentTurn >= this.PlayingUsers().length) this.currentTurn = 0;
+      if(this.currentTurn < 0) this.currentTurn = this.PlayingUsers().length - 1;
+  
+      let playerRound = this.PlayingUsers()[this.currentTurn].value;
+
+      if(lastPass == playerRound){
+        this.emit("match draw");
+        await Timeout(5000);
+        this.Restart();
+        return;
+      }
+
+      if(!playerRound) fnc()
+      else {
+        lastPass = playerRound;
+
+        if(playerRound.spectating) fnc();
+        else{
+          playerRound.serverLogic = {
+            sayUno: false
+          }
+          playerRound.emit("round your", this.currentCard.id());
+        this.emitExclude(playerRound, "round another", playerRound.data.name, this.currentCard.id());
+        if(!first) this.ShowCards(playerRound);
+        }
+      }
+    }
+
+    if(this.currentCard.color == 4 && this.currentCard.number == 4 || this.currentCard.color == 5){
+      //comprou carta e troca
+      let owner = this.currentCard.owner;
+
+      owner.emit("round choose color");
+      this.emitExclude(owner, "round choosing color", owner.data.name);
+      return;
+    }
+
+    await fnc();
+  }
+
+  Draw(socket){
+    if(socket){
+      let drawed = this.GetDeckCard();
+      socket.cards.push(drawed);
+      console.log(socket.cards);
+      socket.emit("draw", drawed.id());
+      return drawed;
+    }
+    return false;
+  }
+
+  Podium(){
+    return this.PlayingUsers().sort((a, b) => {
+      return (a.value.cards.length - b.value.cards.length);
+    });
+  }
+
+  async Win(socket){
+    if(socket){
+      let winners = [];
+
+      let arr = this.Podium();
+
+      for(let i = 0; i < arr.length; i++){
+        winners.push({
+          name: arr[i].value.data.name,
+          cards: arr[i].value.cards.length
+        });
+      }
+
+      this.emit("match win", winners);
+      await Timeout(10000);
+      this.Restart();
+    }
+  }
+
+  NextPlayer(){
+    let futureNewTurn = this.currentTurn + 1;
+    
+      if(futureNewTurn >= this.PlayingUsers().length) futureNewTurn = 0;
+
+    return this.PlayingUsers()[futureNewTurn].value;
+  }
+
+  LastPlayer(){
+    let futureNewTurn = this.currentTurn - 1;
+    
+    if(futureNewTurn < 0) futureNewTurn = this.PlayingUsers().length - 1;
+
+  return this.PlayingUsers()[futureNewTurn].value;
+  }
+
   CanStart(){
+    if(this.started){
+
+      return;
+    }
+
     if(this.users.size >= minUsersStart){
-      let countdown = 2;
-      io.to(this.roomID).emit("starting", countdown);
+      let countdown = countdownToStart;
+      this.emit("starting", countdown);
       let timer = setInterval(() => {
-        if(countdown <= 10) io.to(this.roomID).emit("starting", countdown)
         countdown--;
+        if(countdown <= 10) this.emit("starting", countdown)
         if(countdown == 0) {
           clearInterval(timer);
 
@@ -136,15 +381,20 @@ class Room {
         }
       }, 1000)
     }else{
-      io.to(this.roomID).emit("left", minUsersStart - this.users.size);
+      this.emit("left", minUsersStart - this.users.size);
     }
   }
 
-  GetRandomCard(){
-    let color = randomNumber(0, 3);
-    let number = randomNumber(1, 9);
+  GetDeckCard(){
+    if(!this.roomDeck) this.roomDeck = getRandomDeck();
 
-    return new Carta(color, number);
+    if(this.roomDeckIndex >= this.roomDeck.length) this.roomDeckIndex = 0;
+
+    let card = this.roomDeck[this.roomDeckIndex];
+
+    this.roomDeckIndex++;
+
+    return new Carta(card.color, card.number);
   }
 }
 
@@ -172,6 +422,13 @@ io.on("connection", (socket) => {
 
   currentRoom.JoinUser(uniqueID, socket);
 
+  socket.serverLogic = {
+    sayUno: false
+  }
+
+  socket.on("disconnect", () => {
+    currentRoom.DisconnectUser(socket);
+  })
 
   function IsAValidCard(card){
     if(card.length != 2){
@@ -181,7 +438,7 @@ io.on("connection", (socket) => {
     let color = card[0];
     let number = card[1];
 
-    if(color != "r" && color != "y" && color != "g" && color != "b") return false;
+    if(color != "r" && color != "y" && color != "g" && color != "b" && color != "+" && color != "w") return false;
 
     if(isNaN(number)) return false;
 
@@ -189,17 +446,102 @@ io.on("connection", (socket) => {
 }
 
 function CanPlay(card){
+  if(card[0] == "+" || card[0] == "w") return true;
+
+  if(currentRoom.currentCard.color == 4 && card[0] != "+") return false;
+
   return card[0] == currentRoom.currentCard.id()[0] || card[1] == currentRoom.currentCard.id()[1];
 }
 
-  socket.on("play", (card) => {
+  socket.on("choose color", (nextColor) => {
+    if(nextColor != "b" && nextColor != "y" && nextColor != "r" && nextColor != "g") return;
+    else{
+      currentRoom.emit("choosed color", nextColor, name);
+      currentRoom.currentCard = ConvertCard(nextColor, 0);
+      currentRoom.PassTurn(false);
+    }
+  })
+
+  socket.on("play", async (card) => {
     if(!card) return;
 
     if(card == "draw"){
-      let drawed = currentRoom.GetRandomCard();
-      socket.cards.push(drawed);
-      socket.emit("draw", drawed.id());
+
+      let drawed = currentRoom.Draw(socket);
+      currentRoom.emitExclude(socket, "drawed", name);
       currentRoom.PassTurn(false);
+      return;
+    }
+
+    function ContainsCardInDeck(player, card){
+      let possibleCards = player.cards.filter((a) => {
+        return a.number == card.number && a.color == card.color;
+      })
+
+      return possibleCards.length > 0;
+    }
+
+    function ContainsCardsInDeck(player, card){
+      let possibleCards = player.cards.filter((a) => {
+        return a.number == card.number || a.color == card.color;
+      })
+
+      return possibleCards.length > 0;
+    }
+
+    if(card == "challenge"){
+      if(currentRoom.currentCard.color != 4){
+        socket.emit("cantplay", currentRoom.currentCard.id());
+        return;
+      }
+
+      let lastPlayer = currentRoom.LastPlayer();
+
+      let lastCard = currentRoom.currentCard.lastCard;
+
+      if(lastCard.color == 4) {
+        socket.emit("cantplay", currentRoom.currentCard.id());
+        return;
+      }
+
+      let toBuy;
+
+      if(ContainsCardsInDeck(lastPlayer, lastCard)){
+        currentRoom.emit("challenge win", name, lastPlayer.data);
+        toBuy = lastPlayer;
+      }else {
+        currentRoom.emit("challenge defeat", name, lastPlayer.data);
+        toBuy = socket;
+      }
+
+      await Timeout(2000);
+
+      for(let i = 0; i < 6; i++){
+        currentRoom.Draw(toBuy);
+        currentRoom.emitExclude(socket, "drawed", name);
+        await Timeout(500);
+      }
+
+      currentRoom.currentCard.drawed = true;
+
+      currentRoom.PassTurn(false);
+
+      return;
+    }
+
+    if(card == "show"){
+      currentRoom.ShowCards(socket);
+      socket.emit("showed", currentRoom.currentCard.id());
+      return;
+    }
+
+    if(card == "uno" && socket.cards.length == 2){
+      socket.serverLogic.sayUno = true;
+      socket.emit("uno", currentRoom.currentCard.id());
+      currentRoom.emitExclude(socket, "p uno", name);
+      return;
+    }else if(card == "uno"){
+      socket.emit("cantplay", currentRoom.currentCard.id(), true);
       return;
     }
 
@@ -211,24 +553,98 @@ function CanPlay(card){
     }
 
     let cardObj = ConvertCard(card[0], card[1]);
-
-    let newCardArray = socket.cards.filter((a) => {
-      return !(a.number == cardObj.number && a.color == cardObj.color);
-    });
-
-    socket.cards = newCardArray;
-
-    let everyone = currentRoom.UserArray();
-
-    for(let i = 0; i < everyone.length; i++){
-      let user = everyone[i].value;
-      user.emit("player play", name, cardObj.id());
+    if(!ContainsCardInDeck(socket, cardObj)){
+      socket.emit("cantplay", currentRoom.currentCard.id());
+      return;
     }
+
+    const ClearCard = () => {
+      let cardObj = ConvertCard(card[0], card[1]);
+
+      let jaFoi = false;
+
+      let newCardArray = socket.cards.filter((a) => {
+        if(jaFoi) return true;
+
+        if((a.number == cardObj.number && a.color == cardObj.color)) jaFoi = true;
+
+        return !(a.number == cardObj.number && a.color == cardObj.color);
+      });
+  
+      socket.cards = newCardArray;
+    }
+
+    if(socket.cards.length == 2 && !socket.serverLogic.sayUno){
+      //não disse uno
+
+      if(currentRoom.currentCard.color == 4 && !currentRoom.currentCard.drawed){
+        for(let i = 1; i < currentRoom.currentCard.number; i++){
+          currentRoom.Draw(socket);
+          currentRoom.emitExclude(socket, "drawed", name);
+          await Timeout(500);
+        }
+        currentRoom.currentCard.drawed = true;
+        currentRoom.PassTurn(false);
+        return;
+      }
+      
+      currentRoom.emitExclude(socket, "p didnt uno", name);
+      socket.emit("didnt uno");
+      
+      await Timeout(2000);
+
+      for(let i = 0; i < 2; i++){
+        currentRoom.Draw(socket);
+        await Timeout(750);
+      }
+
+      currentRoom.PassTurn(false);
+
+      return;
+    }
+
+
+    if(card[0] == "+"){
+      let number = parseInt(card[1]);
+
+        let futurePlayer = currentRoom.NextPlayer();
+
+        cardObj.owner = socket;
+
+        ClearCard();
+
+          
+        currentRoom.currentCard = cardObj;
+
+        currentRoom.emit("player play draw", name, cardObj.number, futurePlayer.data);
+
+        await Timeout(2000);
+        
+        for(let i = 1; i <= cardObj.number; i++){
+          currentRoom.Draw(futurePlayer);
+          currentRoom.emitExclude(futurePlayer, "drawed", name);
+          await Timeout(500);
+        }
+
+        if(socket.cards.length > 0)     currentRoom.PassTurn(false);
+        else {
+          currentRoom.Win(socket);
+        }
+        return;
+    }
+
+    ClearCard();
+    cardObj.owner = socket;
 
     currentRoom.currentCard = cardObj;
 
-    currentRoom.PassTurn(false);
+    currentRoom.emit("player play", name, cardObj.id());
+
+    if(socket.cards.length > 0)     currentRoom.PassTurn(false);
+    else {
+      currentRoom.Win(socket);
+    }
   })
 });
 
-httpServer.listen(3000);
+httpServer.listen(5000);
